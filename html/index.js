b85d2302e5f8cdec6a6f3654a5d13a957340d21b
subba123-max <63241136+subba123-max@users.noreply.github.com>
2021-05-26 14:22:19 +0530

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,20 @@
+
+//  function big1(){
+//     var img1= document.getElementById('img1')
+//     img1.width=360;
+//  }
+ 
+//  function small1(){
+//      var img1= document.getElementById('img1')
+     
+//      img1.width=20;
+//   }
+
+
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
